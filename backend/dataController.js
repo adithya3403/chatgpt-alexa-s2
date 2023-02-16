ee04f0293ee996ce80a8d509d1b0c473c23ccb9d
adithya3403 <a.dty.3403@gmail.com>
2023-02-16 10:17:49 +0530

    Updated file

--- backend/dataController.js
+++ backend/dataController.js
@@ -1,6 +1,6 @@
 const Data = require("./dataModel.js");
 
-export const getData = async (req, res) => {
+const getData = async (req, res) => {
     try {
         const data = await Data.find();
         res.json(data);
@@ -9,7 +9,7 @@ export const getData = async (req, res) => {
     }
 };
 
-export const getDataById = async (req, res) => {
+const getDataById = async (req, res) => {
     try {
         const data = await Data.findById(req.params.id);
         res.json(data);
@@ -18,7 +18,7 @@ export const getDataById = async (req, res) => {
     }
 };
 
-export const saveData = async (req, res) => {
+const saveData = async (req, res) => {
     const data = new Data(req.body);
     try {
         const inserteddata = await data.save();
@@ -28,7 +28,7 @@ export const saveData = async (req, res) => {
     }
 };
 
-export const updateData = async (req, res) => {
+const updateData = async (req, res) => {
     try {
         const updateddata = await Data.updateOne({ _id: req.params.id }, { $set: req.body });
         res.status(200).json(updateddata);
@@ -37,7 +37,7 @@ export const updateData = async (req, res) => {
     }
 };
 
-export const deleteData = async (req, res) => {
+const deleteData = async (req, res) => {
     try {
         const deleteddata = await Data.deleteOne({ _id: req.params.id });
         res.status(200).json(deleteddata);
@@ -45,3 +45,5 @@ export const deleteData = async (req, res) => {
         res.status(400).json({ message: error.message });
     }
 };
+
+module.exports = { getData, getDataById, saveData, updateData, deleteData };
